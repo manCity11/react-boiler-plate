ad3d41ba176ec006ec53a37a7eab99297a2b52ca
Inthalak SIRASA <Inthalak.SIRASA@cpexterne.org>
2019-02-20 14:34:18 +0100

    [TECH] Add eslint

--- src/app/commons/page-component/page.component.jsx
+++ src/app/commons/page-component/page.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {I18nProvider} from '@lingui/react';
 
 import {LocalesService} from '../locales/locales';
@@ -13,3 +14,7 @@ export class PageComponent extends React.Component {
     return <I18nProvider i18n={this.i18n}>{this.props.children}</I18nProvider>;
   }
 }
+
+PageComponent.propTypes = {
+  children: PropTypes.object
+};
