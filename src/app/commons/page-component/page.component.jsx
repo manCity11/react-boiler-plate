0c3e096490498c8ad05ce3e4f95dfb14cddcc76e
Inthalak SIRASA <Inthalak.SIRASA@cpexterne.org>
2019-02-20 10:03:40 +0100

    [TECH] Quick cleanup

--- src/app/commons/page-component/page.component.jsx
+++ src/app/commons/page-component/page.component.jsx
@@ -7,11 +7,9 @@ export class PageComponent extends React.Component {
   constructor() {
     super();
     this.i18n = LocalesService.getLocales();
-    console.log('i18n', this.i18n);
   }
 
   render() {
-    console.log(this.i18n);
     return <I18nProvider i18n={this.i18n}>{this.props.children}</I18nProvider>;
   }
 }
--- src/app/init-app.js
+++ src/app/init-app.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import {I18nProvider} from '@lingui/react';
 
-import {App} from './App.jsx';
 import {LocalesService} from './commons/locales/locales';
 import axios from 'axios';
+
+import {App} from './App.jsx';
 
 const initApp = (language = 'fr') => {
   axios
     .get(`dist/locales-${language}.json`)
     .then(({data}) => {
       LocalesService.init({language, locales: data});
-      ReactDOM.render(<App/>, document.getElementById("root"));
+      const i18n = LocalesService.getLocales();
+      ReactDOM.render(<I18nProvider i18n={i18n}>
+        <App/>
+      </I18nProvider>, document.getElementById("root"));
     });
 }
 
