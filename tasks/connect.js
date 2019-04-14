e3adbf2fe13343e30e75fbae26a79574dfc3dc3f
Inthalak SIRASA <inthalak.sirasa@gmail.com>
2019-04-14 12:51:19 +0200

    [TECH] Add history fallback

--- tasks/connect.js
+++ tasks/connect.js
@@ -1,17 +1,20 @@
 module.exports = () => {
-  const webpack = require('webpack');
-  const WebpackDevServer = require('webpack-dev-server');
-  const gulp = require('gulp');
-  const {port} = require('./build-config')
-  let config = require('./webpack.config')();
+  const webpack = require("webpack");
+  const WebpackDevServer = require("webpack-dev-server");
+  const gulp = require("gulp");
+  const { port } = require("./build-config");
+  let config = require("./webpack.config")();
 
+  config.entry.app.unshift(
+    `webpack-dev-server/client?http://localhost:${port}/`,
+    "webpack/hot/dev-server"
+  );
 
-  config.entry.app.unshift(`webpack-dev-server/client?http://localhost:${port}/`, "webpack/hot/dev-server");
-  
   const compiler = webpack(config);
   const server = new WebpackDevServer(compiler, {
-    hot: true
+    hot: true,
+    historyApiFallback: true
   });
   server.listen(port);
-  gulp.watch('src/**/locales/[A-z]*.json', gulp.series('merge-locales'));
+  gulp.watch("src/**/locales/[A-z]*.json", gulp.series("merge-locales"));
 };
